434ea69aa275f22d250986bc81a7ff65ac2e3b41
Ashf5 <friedash85@gmail.com>
2025-10-21 17:30:30 +0300

    add item cards

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+
+import Navbar from "./components/Navbar/Navbar";
+import Homepage from "./pages/Homepage";
+
+
+function App() {
+
+  return (
+    <>
+      <Homepage />
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+
+import './header.css';
+
+const Header:React.FC<{message:string}> = (props) => {
+    return (
+        <div className='header-container'>
+            <h1 className='header-text'>{props.message}</h1>
+            <a className='header-icon'><PersonOutlineOutlinedIcon /></a>
+        </div>
+    )
+}
+
+export default Header;
