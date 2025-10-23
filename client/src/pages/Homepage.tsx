2512cbb6fcb2ec117c8ae70a4f5158c3c681b478
Ashf5 <friedash85@gmail.com>
2025-10-23 17:17:54 +0300

    connect homepage to api

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,7 +1,9 @@
 
 import SaleCard from "../features/SaleCard"
-
+import { BASE_API_URL } from "../constants";
+import type { Sale } from "../types/saleTypes";
 import './homepage.css'
+import { useEffect, useState } from "react";
 
 const images = [
     "/missing-photo.jpg",
@@ -13,16 +15,26 @@ const images = [
 
 const profilePicture = 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=2048x2048&w=is&k=20&c=8QovDK9XochFpaIC-N3pn5EEaRSVuE1SKpQDVUxLSUk='
 
+
 const Homepage:React.FC = () => {
+    const [sales, setSales] = useState<Sale[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchSales = async () => {
+            const data = await fetch(BASE_API_URL + '/sales', {credentials: 'include'});
+            const jsonData = await data.json();
+            setSales(jsonData.sales);
+            setLoading(false);
+        }
+
+        fetchSales();
+    }, [])
 
     return (
         <div className="homepage-main">
-            <SaleCard profilePic={profilePicture} sellerName="asher" date="12/12/2025" pictures={images}/>
-            <SaleCard profilePic={profilePicture} sellerName="asher" date="12/12/2025" pictures={images}/>
-            <SaleCard profilePic={profilePicture} sellerName="asher" date="12/12/2025" pictures={images}/>
-            <SaleCard profilePic={profilePicture} sellerName="asher" date="12/12/2025" pictures={images}/>
-            <SaleCard profilePic={profilePicture} sellerName="asher" date="12/12/2025" pictures={images}/>
-            <SaleCard profilePic={profilePicture} sellerName="asher" date="12/12/2025" pictures={images}/>
+            {/* <SaleCard profilePic={profilePicture} sellerName="asher" date="12/12/2025" pictures={images}/> */}
+            {!loading && sales.map(sale => <SaleCard profilePic={sale.profile_picture || undefined} sellerName={sale.firstname} date={sale.date_created} pictures={sale.images}/>)}
         </div>
     )
 }
