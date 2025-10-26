80c6ebfb8fc2e647a79550af3a2fa4f19290e760
Ashf5 <friedash85@gmail.com>
2025-10-26 16:59:03 +0200

    add login functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/Loginpage";
+import { AuthProvider } from "./context/authContext";
 
 
 function App() {
@@ -11,13 +12,14 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <Header message="Active Sales" />
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/login' element={<LoginPage />} />
-        </Routes>
-
-        <Navbar />
+        <AuthProvider>
+          <Header message="Active Sales" />
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/login' element={<LoginPage />} />
+          </Routes>
+          <Navbar />
+        </AuthProvider>
       </BrowserRouter>
 
 
--- client/src/features/LoginForm/LoginForm.tsx
+++ client/src/features/LoginForm/LoginForm.tsx
@@ -1,10 +1,44 @@
+import { useAuth } from "../../context/authContext";
 
+import { BASE_API_URL } from "../../constants";
+import { useNavigate } from "react-router-dom";
 
 
 const LoginForm:React.FC = () => {
+    const {login} = useAuth();
+    const nav = useNavigate()
+
+    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get('email');
+        const password = formData.get('password');
+
+        const response = await fetch(BASE_API_URL + '/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                email, 
+                password
+            })
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            login(data.accessToken);
+            nav('/');
+        }
+        else {
+            alert('Error signing in')
+        }
+
+
+    }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor='email'>Email</label><br/>
             <input type="email" name="email" placeholder="you@example.com" required/><br/>
             <label htmlFor="Password">Password</label><br/>
