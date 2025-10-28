1540238463816fd3573f1354c313a025173c0b3a
Ashf5 <friedash85@gmail.com>
2025-10-28 18:27:44 +0200

    add items component

---
+++ client/src/pages/Itemspage.tsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import SaleDetailsCard from "../features/SaleDetailsCard/SaleDetailsCard";
+
+const Itemspage: React.FC = () => {
+    const 
+
+    useEffect(() => {
+        // get sales and items
+
+    }, [])
+
+    return (
+        <div>
+            {/* <SaleDetailsCard /> */}
+        </div>
+    )
+}
+
+export default Itemspage;
